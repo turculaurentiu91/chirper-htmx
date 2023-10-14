aa948c1f886fdefff5cdc0673a270d4772a12e15
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-10-14 11:21:04 +0200

    added aria label

--- resources/views/components/chirps/create.blade.php
+++ resources/views/components/chirps/create.blade.php
@@ -9,6 +9,7 @@
 >
     @csrf
     <textarea
+        aria-label="message"
         name="message"
         placeholder="{{ __('What\'s on your mind?') }}"
         class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
